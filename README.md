f7b7f1cce5c5c252d6d5752ebea63531f7d90e0e
Rodrigo Mayer <mayerrodrigo98@gmail.com>
2023-04-26 18:01:17 -0300

    Add task on enter key

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 TODO App based on [Rocketseat's React Course - Challenge 1](https://efficient-sloth-d85.notion.site/Desafio-01-Praticando-os-conceitos-do-ReactJS-91fd63dd1a5b4a2796152de293ec1074)
 
+You can check the app here:
+
+[https://todo-app-rodrigo98rm.vercel.app/](https://todo-app-rodrigo98rm.vercel.app/)
+
 # Design
 
 Rocketseat's design available on [Figma](https://www.figma.com/file/eMRaOdRfG42qu1nldQgPfh/ToDo-List-(Rocketseat)?node-id=0%3A1&t=xLcRb5D5GM6ipOVd-1)
@@ -11,7 +15,6 @@ Rocketseat's design available on [Figma](https://www.figma.com/file/eMRaOdRfG42q
 
 - Animate task list items when a task status is changed (https://medium.com/ft-product-technology/animating-list-reordering-with-react-hooks-1aa0d78a24dc)
 - Move completed task to the top of completed tasks (not the bottom)
-- Add task by pressing Enter on the keyboard
 - Save tasks to browser's local storage
 - Refactor TaskList Component
 
--- src/components/CreateTask/index.tsx
+++ src/components/CreateTask/index.tsx
@@ -1,12 +1,14 @@
 import { PlusCircle } from '@phosphor-icons/react';
 import styles from './styles.module.css';
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useState, KeyboardEvent, useRef } from 'react';
 
 interface CreateTaskProps {
   addTask: (description: string) => void;
 }
 
 export function CreateTask({ addTask }: CreateTaskProps) {
+  const formRef = useRef(null);
+
   const [description, setDescription] = useState('');
 
   function handleDescriptionChange(e: ChangeEvent<HTMLTextAreaElement>) {
@@ -21,10 +23,21 @@ export function CreateTask({ addTask }: CreateTaskProps) {
     setDescription('');
   }
 
+  function handleKeyPress(e: KeyboardEvent<HTMLTextAreaElement>) {
+    console.log(e.key);
+    console.log(e.shiftKey);
+
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      formRef.current.requestSubmit();
+    }
+  }
+
   const isNewTaskEmpty = !description;
 
   return (
     <form
+      ref={formRef}
       className={styles.container}
       onSubmit={(e) => {
         handleCreateTask(e);
@@ -35,6 +48,7 @@ export function CreateTask({ addTask }: CreateTaskProps) {
         onChange={handleDescriptionChange}
         value={description}
         required
+        onKeyDown={handleKeyPress}
       ></textarea>
       <button type="submit" disabled={isNewTaskEmpty}>
         Criar <PlusCircle size={20} />
